6b11117f265f3298a39f1d0ac24ab92fbf53d1f0
ctamario <ctamario@gmail.com>
2024-02-27 15:59:55 +0100

    update

--- script.R
+++ script.R
@@ -86,7 +86,7 @@ leaflet() %>% addProviderTiles("Esri.WorldImagery") %>%
 
 #joined_data <- st_join(dams_sf_wgs84, df_sf_wgs84, join = st_nearest_feature, dist = 100)
 
-dams_related <- joined_data[which(joined_data$RSTID %in% up_seg_all2(df, one_segment)),]
+#dams_related <- joined_data[which(joined_data$RSTID %in% up_seg_all2(df, one_segment)),]
 
 leaflet() %>% addProviderTiles("Esri.WorldImagery") %>%
   addPolylines(data=one_segment_sf, popup=~RW_PopNamn) %>%
@@ -273,6 +273,22 @@ write_sf(obj=keep, dsn="data/kanadaroding.shp")
 nordhavet <- read_sf("data/nordhavet_mynning.shp")
 nordhavet
 
+up_seg_all4 <- function(in_df, seg, damLayer){
+  next_up <- up_seg(in_df, seg)
+  list_up <- next_up
+  while(length(next_up) > 0) {
+    new_up <- up_seg(in_df, next_up[1])
+    new_up <- new_up[!new_up %in% list_up]  # Remove segments already in list_up
+    #new_up <- sapply(new_up, function(x) !any(dams_related$RSTID == x))
+    list_up <- c(list_up, new_up)
+    new_up <- new_up[!new_up %in% damLayer$RSTID]
+    
+    next_up <- c(next_up, new_up)
+    next_up <- next_up[-1]
+  }
+  return(c(seg, list_up))
+} # Det funkar!
+
 nordhavet2 <- st_join(nordhavet, df_sf_wgs84, join = st_nearest_feature, dist = 100)
 
 one_segment_nordhavet <- nordhavet2$RSTID[1]
@@ -281,7 +297,7 @@ down_segment_sf2 <-  df_sf_wgs84[which(df$RSTID %in% down_seg_until_dam(df, one_
 
 furthest_down <- down_seg_until_dam(df, one_segment_nordhavet)$furthest_down
 
-up_all_segment_sf3 <- df_sf_wgs84[which(df$RSTID %in% up_seg_all3(df, furthest_down)),] 
+up_all_segment_sf3 <- df_sf_wgs84[which(df$RSTID %in% up_seg_all4(df, one_segment_nordhavet, joined_data)),] 
 
 test <- do_it_all(df, one_segment_nordhavet)
 
@@ -294,10 +310,87 @@ leaflet() %>% addProviderTiles("Esri.WorldImagery") %>%
   #addCircleMarkers(data=df_lake_trout_joined2, fillColor="pink", color="red", fillOpacity = 1,  radius = 3)
 
 
-67574770355019
-67306520369160
+### Do it with two different dam layers!
 
-hm1 <- c(TRUE, FALSE)
-hm2 <- c(TRUE, FALSE)
 
-hm1 + hm2
+# Clean dam layer:
+
+dams_clean <- joined_data %>% 
+  filter(STATUS != 2) %>% 
+  filter(VHINDER != 2)
+
+table(dams_clean$FISKVAG)
+
+# Load in fishways: 
+
+AIV <- read_sf(dsn="data/AtgarderIVatten.shp")
+
+AIV_wgs84 <- st_transform(AIV, crs = st_crs(4326))
+
+table(AIV$HuvAtgtyp)
+
+AIV_clean <- AIV_wgs84 %>% filter(HuvAtgtyp == "Fiskvägar")  %>% filter(Nkoord > 6108110)
+
+# Connect AIV to dam layer
+
+dams_w_AIV <- st_join(dams_clean, AIV_clean, join = st_nearest_feature, dist = 10)
+
+#dams_w_AIV <- st_nearest_feature(dams_clean, AIV_clean)
+
+#dist = st_distance(dams_clean, AIV_clean[st_nearest_feature(dams_clean, AIV_clean),], by_element=TRUE)
+
+#dams_w_AIV$dist <- dist
+
+#dams_w_AIV %>% filter(dist < 100)
+
+p_load(nngeo)
+
+hm <- st_nn(dams_clean, AIV_clean, k = 1, returnDist = T)
+hm2 <- cbind(dams_w_AIV, unlist(hm$dist))
+
+hm3 <- hm2 %>% filter(unlist.hm.dist. < 100)
+
+hm3
+
+dams_w_FPS <- dams_clean %>% filter(!DAMMID %in% hm3$DAMMID)
+
+####################
+
+
+# Without fishways
+conn_wo_FPS <- df_sf_wgs84[which(df$RSTID %in% up_seg_all4(df, one_segment_nordhavet, dams_clean)),] 
+
+# With fishways
+conn_w_FPS <- df_sf_wgs84[which(df$RSTID %in% up_seg_all4(df, one_segment_nordhavet, dams_w_FPS)),] 
+
+
+leaflet() %>% addProviderTiles("Esri.WorldImagery") %>% 
+  #addCircleMarkers(data=df_lake_trout_joined2 %>% filter(RSTID == df_lake_trout_joined$RSTID[ID]), fillColor="pink", color="red", fillOpacity = 1,  radius = 10) %>%
+  addPolylines(data=conn_w_FPS %>% filter(LINJEKOD < 26 & SWE > 0), color="white", popup=~paste(RSTID, RW_PopNamn, sep="<br>"), weight=2) %>%
+  addPolylines(data=conn_wo_FPS %>% filter(LINJEKOD < 26 & SWE > 0), color="red", popup=~paste(RSTID, RW_PopNamn, sep="<br>"), weight=2)
+#  addCircleMarkers(data = joined_data %>% filter(RSTID %in% up_all_segment_sf3$RSTID) %>%
+#                     filter(RSTID != 67574770355019) %>% filter(RSTID != 67306520369160), popup=~paste(RSTID), radius = 2, weight=2, fillOpacity = 1, fillColor="white")# %>% 
+#addCircleMarkers(data=df_lake_trout_joined2, fillColor="pink", color="red", fillOpacity = 1,  radius = 3)
+
+
+df_sf_wgs84$line.length <- st_length(df_sf_wgs84)
+sum(df_sf_wgs84$line.length)
+
+conn_wo_FPS_clean <- conn_wo_FPS %>% filter(LINJEKOD < 26 & SWE > 0)
+conn_w_FPS_clean <- conn_w_FPS %>% filter(LINJEKOD < 26 & SWE > 0)
+
+conn_wo_FPS_clean$line.length <- st_length(conn_wo_FPS_clean)
+sum(conn_wo_FPS_clean$line.length)
+
+conn_w_FPS_clean$line.length <- st_length(conn_w_FPS_clean)
+sum(conn_w_FPS_clean$line.length)
+
+sum(conn_wo_FPS_clean$line.length)/sum(df_sf_wgs84$line.length)
+sum(conn_w_FPS_clean$line.length)/sum(df_sf_wgs84$line.length)
+#Från 10.8% av Sveriges vattendrag till 11.7%,
+
+sum(conn_w_FPS_clean$line.length)/sum(conn_wo_FPS_clean$line.length)
+#En ökning av tillgängligt habitat på 8%
+
+
+
